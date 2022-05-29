d60151c7690613e4d4e7d66c60b6dfb85d50bdcd
Mathijs van der Schoot <mathijsvdschoot@gmail.com>
2022-05-29 18:38:14 +0200

    ..

--- src/Program.cpp
+++ src/Program.cpp
@@ -13,18 +13,17 @@ namespace LuigiMaker
 
 		virtual void OnAttach()
 		{
+			CH_PROFILE_FUNCTION();
 			_shaderlib.addShader(_shader_loc, _shader_name);
 
 			float vertices[4 * 7] = {
-				-0.5f,-0.5f,0.0f, 	0.0f, 0.0f,
-				0.5f,-0.5f,0.0f,	1.0f, 0.0f,
-				-0.5f,0.5f,0.0f,	0.0f, 1.0f,
-				0.5f,0.5f,0.0f,		1.0f, 1.0f
-			};
+				-0.5f, -0.5f, 0.0f, 0.0f, 0.0f,
+				0.5f, -0.5f, 0.0f, 1.0f, 0.0f,
+				-0.5f, 0.5f, 0.0f, 0.0f, 1.0f,
+				0.5f, 0.5f, 0.0f, 1.0f, 1.0f};
 			std_int indices[6] = {
 				0, 1, 2,
-				1, 2, 3
-			};
+				1, 2, 3};
 
 			_pos = glm::vec3(0.0f, 0.0f, 1.0f);
 
@@ -32,62 +31,62 @@ namespace LuigiMaker
 
 			_texture = Cheets::Texture2D::Create(_texture_loc);
 			_texture->bind(0);
+			_shaderlib.getShader(_shader_name)->bind();
 			_shaderlib.getShader(_shader_name)->setUniform1i("text", 0);
 
 			Cheets::Ref<Cheets::VertexBuffer> _vertex_buffer = Cheets::VertexBuffer::Create(vertices, sizeof(vertices));
 			Cheets::Ref<Cheets::IndexBuffer> _index_buffer = Cheets::IndexBuffer::Create(indices, 6);
 
 			Cheets::BufferLayout layout =
-			{
-				Cheets::BufferElement{"a_Position", Cheets::ShaderDataType::Float3},
-				Cheets::BufferElement{"a_uv", Cheets::ShaderDataType::Float2}
-			};
+				{
+					Cheets::BufferElement{"a_Position", Cheets::ShaderDataType::Float3},
+					Cheets::BufferElement{"a_uv", Cheets::ShaderDataType::Float2}};
 
 			_vertex_buffer->setBufferLayout(layout);
 
 			_vertex_array = Cheets::VertexArray::Create();
 			_vertex_array->addVertexBuffer(_vertex_buffer);
 			_vertex_array->setIndexBuffer(_index_buffer);
-		
+
 			_camera = Cheets::createRef<Cheets::OrthographicCamera>(-1.0f, 1.0f, -1.0f, 1.0f,
-					_pos, 0.0f);
+																	_pos, 0.0f);
 		}
 		virtual void OnDetach()
 		{
 		}
 		virtual void OnEvent(Cheets::Event &event)
 		{
-			//APP_INFO("Event: {}", event.GetName());
-			//Cheets::EventDispatcher e(event);
-			//e.Dispatch<Cheets::KeyPressedEvent>(BIND_EVENT_FN(SomeLayer::onKeyPressed));
+			// APP_INFO("Event: {}", event.GetName());
+			// Cheets::EventDispatcher e(event);
+			// e.Dispatch<Cheets::KeyPressedEvent>(BIND_EVENT_FN(SomeLayer::onKeyPressed));
 		}
 
 		void isKeyPressed(Cheets::Timestep ts)
 		{
 			float dt = ts.getSeconds();
-			if(Cheets::Input::isKeyPressed(Cheets::Key::A))
+			if (Cheets::Input::isKeyPressed(Cheets::Key::A))
 			{
 				_pos.x -= _speed * dt;
 			}
-			if(Cheets::Input::isKeyPressed(Cheets::Key::D))
-			{	
+			if (Cheets::Input::isKeyPressed(Cheets::Key::D))
+			{
 				_pos.x += _speed * dt;
 			}
 
-			if(Cheets::Input::isKeyPressed(Cheets::Key::W))
-			{	
+			if (Cheets::Input::isKeyPressed(Cheets::Key::W))
+			{
 				_pos.y += _speed * dt;
 			}
 
-			if(Cheets::Input::isKeyPressed(Cheets::Key::S))
-			{	
+			if (Cheets::Input::isKeyPressed(Cheets::Key::S))
+			{
 				_pos.y -= _speed * dt;
 			}
-			if(Cheets::Input::isKeyPressed(Cheets::Key::Tab))
+			if (Cheets::Input::isKeyPressed(Cheets::Key::Tab))
 			{
 				_pos.z -= _speed * dt;
 			}
-			if(Cheets::Input::isKeyPressed(Cheets::Key::CapsLock))
+			if (Cheets::Input::isKeyPressed(Cheets::Key::CapsLock))
 			{
 				_pos.z += _speed * dt;
 			}
@@ -95,6 +94,7 @@ namespace LuigiMaker
 
 		virtual void OnUpdate(Cheets::Timestep ts)
 		{
+			CH_PROFILE_FUNCTION();
 			Cheets::RendererCommand::ClearColor({1.0f, 0.1f, 0.1f, 1.0f});
 			Cheets::RendererCommand::clear();
 
@@ -102,7 +102,7 @@ namespace LuigiMaker
 			_camera->setPosition(_pos);
 			_camera->setRotation(_rot);
 			Cheets::Renderer::beginScene(*_camera);
-			
+
 			Cheets::Renderer::submit(_shaderlib.getShader(_shader_name), _vertex_array);
 
 			Cheets::Renderer::endScene();
